1f833a823b23ab7c4799d794f0c1d37e7571fd24
Juan Lopez <juan07alberto@gmai.com>
2020-05-11 16:31:54 -0500

    navbar ready

--- Login_Sign-In/js/login.js
+++ Login_Sign-In/js/login.js
@@ -1,8 +1,21 @@
 function login(){
-    if($('#email').val() != 'admin@' && $('#password').val() != "password"){
-        $('#msg').attr('hidden', false);
+    let config = {
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            "Access-Control-Allow-Origin": "*",
+        }
+    };
+    axios.post('http://localhost:3000/vd/login', {email: $('#email').val(), contrasena: $('#password').val()}, config).then( res => {
+        console.log(res.data.token)
+    }).catch( err => {
+        console.log(err)
+        showMsg(err)
+    })
+}
+function showMsg(msg){
+    $('#msg').attr('hidden', false);
         setTimeout( () => {
+            $('#msg').html(msg)
             $('#msg').attr('hidden', true);
-        },2000);
-    }
+    },2000);
 }

b000910f86f92524291e9fdab208677cddf7000c
Juan Lopez <juan07alberto@gmai.com>
2020-05-20 10:45:41 -0500

    Ordenado

--- Login_Sign-In/js/login.js
+++ Login_Sign-In/js/login.js
@@ -1,16 +1,21 @@
 function login(){
-    let config = {
-        headers: {
-            'Content-Type': 'application/json;charset=UTF-8',
-            "Access-Control-Allow-Origin": "*",
-        }
-    };
-    axios.post('http://localhost:3000/vd/login', {email: $('#email').val(), contrasena: $('#password').val()}, config).then( res => {
-        console.log(res.data.token)
-    }).catch( err => {
-        console.log(err)
-        showMsg(err)
-    })
+    // let config = {
+    //     headers: {
+    //         'Content-Type': 'application/json;charset=UTF-8',
+    //         "Access-Control-Allow-Origin": "*",
+    //     }
+    // };
+    // axios.post('http://localhost:3000/vd/login', {email: $('#email').val(), contrasena: $('#password').val()}, config).then( res => {
+    //     console.log(res.data.token)
+    // }).catch( err => {
+    //     console.log(err)
+    //     showMsg(err)
+    // })
+    if($('#email').val() == 'admin@' && $('#password').val() == 'password'){
+        location.href='../Servicios/servicios.html'
+    }else{
+        showMsg()
+    }
 }
 function showMsg(msg){
     $('#msg').attr('hidden', false);
